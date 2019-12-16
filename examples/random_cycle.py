0a811b2e4f653f8d2238444ed4c1237134b14888
justDaBuK <kevinjust87@gmail.com>
2019-12-16 18:51:06 +0100

    adjusted initial state

--- examples/random_cycle.py
+++ examples/random_cycle.py
@@ -1,11 +1,12 @@
 from tree import RGBXmasTree
-from colorzero import Hue
+from colorzero import Color, Hue
 from random import randint
 
 tree = RGBXmasTree(brightness=0.05)
+colors = [Color('red'), Color('green'), Color('blue')]  # add more if you like
 
 for pixel in tree:
-    pixel.color = Hue(deg=randint(0, 360))
+    pixel.color = colors[randint(0, 2)]
 
 while True:
     for pixel in tree:
